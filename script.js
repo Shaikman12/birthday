6cf9e1515efebe12d5a37eb6eba7915d8788a373
Shaikman12 <termination58@gmail.com>
2026-02-20 19:38:11 +0530

    Add functions to start movie and show message

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+function startMovie() {
+    document.getElementById("intro").style.display = "none";
+    document.getElementById("main").style.display = "flex";
+}
 
+function showMessage() {
+    document.getElementById("finalMessage").style.display = "block";
+}
